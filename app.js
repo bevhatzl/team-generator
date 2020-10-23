64f4797f058e067a01135df43a640c97ad46baa7
Bev Hatzl <61686101+bevhatzl@users.noreply.github.com>
2020-10-23 17:44:12 +1100

    tests passing

--- app.js
+++ app.js
@@ -83,6 +83,7 @@ async function getEmpDetails() {
             const answers = await inquirer.prompt(engineerQuestions);
             // Create instance of the Engineer class
             let emp = new Engineer(answers.name, answers.id, answers.email, answers.gitHub);
+            console.log("Here is Emp details: " + JSON.stringify(emp));
             // Add to employee array
             employeeArray.push(emp);
         } else {
